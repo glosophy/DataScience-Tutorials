7b69e3c7f2c176d0d49fc71f5f4fe4f096324e7e
glosophy <suttersg@gmail.com>
2020-09-19 16:44:05 -0400

    Update html table

--- Scrape-HTML-Table.py
+++ Scrape-HTML-Table.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from unicodedata import normalize
 
+# ERROR: urllib.error.URLError: <urlopen error [SSL: CERTIFICATE_VERIFY_FAILED] certificate verify failed (_ssl.c:833)>
 # Solution if you are on OSX and Python 3.6: https://stackoverflow.com/questions/50236117/scraping-ssl-certificate-verify-failed-error-for-http-en-wikipedia-org
 inflation_tables = pd.read_html('https://en.wikipedia.org/wiki/List_of_countries_by_inflation_rate')
 
@@ -20,13 +21,20 @@ print(df.head())
 print(df.info())
 
 # set Inflation Rate column to float
+infl_rate = []
+for i in df['Inflation rate (consumer prices) (%)']:
+    if '−' in i:
+        i = i.replace('−', '-')
+    if '-' in i:
+        i = i.replace('-', '-')
+    i = float(i)
+    infl_rate.append(i)
 
-
-#df['Inflation rate (consumer prices) (%)'] = df['Inflation rate (consumer prices) (%)'].astype('float')
+df['Inflation rate (consumer prices) (%)'] = infl_rate
 
 
 # set Data column to int. Get rid of 'est.'
-df['Date of information'] = df['Date of information'].replace({'est.':''}, regex=True).astype('int64')
+#df['Date of information'] = df['Date of information'].replace({'est.':''}, regex=True).astype('int64')
 
 
 
